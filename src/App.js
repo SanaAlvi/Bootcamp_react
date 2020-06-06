e36ce778936ca9090d131fa57ddbc96d57811706
SanaAlvi <sanaalvi066@gmail.com>
2020-06-06 23:17:21 +0500

    Update App.js
    Hello World

--- src/App.js
+++ src/App.js
@@ -5,20 +5,7 @@ import './App.css';
 function App() {
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Welcome from Sana Alvi
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn From Sana Alvi 
-        </a>
-      </header>
+      <h1>Hello World</h1>
     </div>
   );
 }
