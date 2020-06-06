878707b5cd3002c93d92a948d8b0bad80efdcac4
SanaAlvi <sanaalvi066@gmail.com>
2020-06-06 23:44:01 +0500

    Update App.js

--- src/App.js
+++ src/App.js
@@ -1,11 +1,10 @@
 import React from 'react';
-
 import './App.css';
 
 function App() {
   return (
     <div className="App">
-      <h1>Hello World</h1>
+      <h1>Hello World From Alvi</h1>
     </div>
   );
 }
