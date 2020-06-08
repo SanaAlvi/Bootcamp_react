e464493c79175848405896e1de3c5f3b75b31549
SanaAlvi <sanaalvi066@gmail.com>
2020-06-08 11:51:49 +0500

    Update App.js

--- src/App.js
+++ src/App.js
@@ -4,7 +4,10 @@ import './App.css';
 function App() {
   return (
     <div className="App">
+    <body style="backgrondcolor:purple">
       <h1>Hello World From Alvi</h1>
+    </body>
+      
     </div>
   );
 }
