bbc20843e5f0e0fdcf3d5741b24b700eef3a3790
SanaAlvi <sanaalvi066@gmail.com>
2020-06-13 14:32:41 +0500

    Update App.js

--- src/App.js
+++ src/App.js
@@ -3,16 +3,16 @@ import React, {useState} from 'react';
 import Change from './Changer.js'
 function App() {
 
-  let [Value ValueSet]= ValueSet()
+  let [Value,ValueSet]= ValueSet()
 
   return (
     <div className="App">
-      <header className="App-header">
+  
         <p>
           Welcome from Sana Alvi
         </p>
         <Change Changer= {Value} />
-      </header>
+ 
     </div>
   );
 }
