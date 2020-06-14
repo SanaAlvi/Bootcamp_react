5b5d2830a5f7882d2730be52a26d433391d6ba5c
SanaAlvi <sanaalvi066@gmail.com>
2020-06-14 20:41:06 +0500

    Update App.js
    if else condition added

--- src/App.js
+++ src/App.js
@@ -7,14 +7,15 @@ function App() {
   let [Message,setMessage]= useState(true)
 
   return (
-    <div className="App">
+    <div className="App-header">
   
         <p>
           Welcome from Sana Alvi
         </p>
         <Change Changer= {Value} />
         <button onClick={() => ValueSet(Value + 1)}>Chlick Me</button>
-  <h1>Hello {Message ? 'You are Right' : 'You may be Wrong'}</h1>
+        <button onClick={ () => ValueSet(Value = 0)}>Reset Me</button>
+  <h1>Hello {Message ? "You are Right" : "You may be Wrong"}</h1>
       <button onClick={() => setMessage=(!setMessage)}>Change Message</button>
  
     </div>
