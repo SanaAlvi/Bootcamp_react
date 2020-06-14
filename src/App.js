5556934ddbbfd33bdf0e6a3436463bd527ef203a
SanaAlvi <sanaalvi066@gmail.com>
2020-06-14 20:09:04 +0500

    Update App.js
    New Button added

--- src/App.js
+++ src/App.js
@@ -4,6 +4,7 @@ import Change from './Changer.js'
 function App() {
 
   let [Value,ValueSet] = useState(0)
+  let [Message,setMessage]= useState(true)
 
   return (
     <div className="App">
@@ -12,7 +13,9 @@ function App() {
           Welcome from Sana Alvi
         </p>
         <Change Changer= {Value} />
-        <button onClick={() => Value=(ValueSet+1)} />
+        <button onClick={() => ValueSet=(Value+1)}>Chlick Me</button>
+  <h1>Hello {setMessage ? 'You are Right' : 'You may Wronge'}</h1>
+      <button onClick={() => Message=(!Message)}>Change Message</button>
  
     </div>
   );
