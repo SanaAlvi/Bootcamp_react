2c4256ce703e49c5d88d5922d2202480268e2065
SanaAlvi <sanaalvi066@gmail.com>
2020-06-14 20:48:53 +0500

    Update Changer.js

--- src/Changer.js
+++ src/Changer.js
@@ -3,13 +3,8 @@ import React from 'react';
 function Change(props) {
 return (
     <div>
-    
-            if(Changer === 2){
-                <h3>Hello {props.Changer} </h3>
-            }
-            else {
-                <h3>Counter Value is {props.Changer} </h3>
-            }
+       <h3>Hello {props.Changer} </h3>
+       <h3>Counter Value is {props.Changer} </h3>
     </div>
 )
 }
